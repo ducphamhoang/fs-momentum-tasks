2a813430211966987d4eb2eb5e984cae4e25593d
Duc Pham <ducpham.idc@gmail.com>
2025-11-03 15:45:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9429142716-fa395",
+  "appId": "1:772923294665:web:53ce29dba05e9625705e18",
+  "apiKey": "AIzaSyBYtsXQKjnYp0BAlIwaG2AqTdbmKQUdugU",
+  "authDomain": "studio-9429142716-fa395.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "772923294665"
+};
